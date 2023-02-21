8ed338826bb3aa7bf5e5436820013687b062b909
Abood Afifi <38721478+AEM5299@users.noreply.github.com>
2023-02-21 13:00:09 +1100

    bug report

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Mail\MailableA;
+use App\Mail\MailableB;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +18,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test', function () {
+    $mailableA = new MailableA();
+    $mailableB = new MailableB();
+
+    logger('-------------------------------------------');
+    logger('email A');
+    Mail::mailer('log')->to('test@email.com')->send($mailableA);
+
+    logger('-------------------------------------------');
+    logger('email B');
+    Mail::mailer('log')->to('test@email.com')->send($mailableB);
+
+    return;
 });
